f43e14a0e7ea336961b8b861fdde3ccd5ae237e4
George Raafat <georgexraafat@gmail.com>
2024-01-22 18:50:32 -0800

    doing some task

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,12 +5,20 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-logs = client.logs.nginx
-print(f"{logs.count_documents({})} logs")
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in http_methods:
-    count = logs.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
-status = logs.count_documents({"method": "GET", "path": "/status"})
-print(f"{status} status check")
+def log_stats():
+    """
+    provides some stats about Nginx logs stored in MongoDB
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    print(f"{logs.count_documents({})} logs")
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in http_methods:
+        count = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    status = logs.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
